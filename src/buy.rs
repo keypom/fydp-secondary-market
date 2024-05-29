18863797f1bfc6895074cc18f7105d995cb971e5
BenKurrek <bkurrek@uwaterloo.ca>
2024-05-29 15:54:53 -0400

    buildable

--- src/buy.rs
+++ src/buy.rs
@@ -478,8 +478,7 @@ impl Marketplace {
                     Some(vec![seller_new_linkdrop_pk.clone()]),
                     U128(ticket_price),
                     Some(seller_linkdrop_drop_id.clone()),
-                )
-                .then(Self::ext(env::current_account_id())
+                );
             }
         }
     }
